87c6a586e49a9c8fb42b14988a10bedffd55a3b5
km20180269 <40146720180269@litec.ac.at>
2021-03-03 09:53:29 +0100

    add switch to sections

--- script/script.js
+++ script/script.js
@@ -1,3 +1,33 @@
 window.addEventListener('load', () => {
-  
+    document.getElementById('userIcon').addEventListener('click', () => {
+        hideAll();
+        document.getElementById('signInWrapper').style.display = 'flex';
+    })
+
+    document.getElementById('goToSounds').addEventListener('click', () => {
+        hideAll();
+        document.getElementById('soundboardWrapper').style.display = 'flex';
+    })
+
+    document.getElementById('goToNewSounds').addEventListener('click', () => {
+        hideAll();
+        document.getElementById('uploadWrapper').style.display = 'flex';
+    })
+
+    document.getElementById('goToSignUp').addEventListener('click', () => {
+        hideAll();
+        document.getElementById('signUpWrapper').style.display = 'flex';
+    })
+
+    document.getElementById('goToSignIn').addEventListener('click', () => {
+        hideAll();
+        document.getElementById('signInWrapper').style.display = 'flex';
+    })
 })
+
+function hideAll() {
+    document.getElementById('signInWrapper').style.display = 'none';
+    document.getElementById('signUpWrapper').style.display = 'none';
+    document.getElementById('soundboardWrapper').style.display = 'none';
+    document.getElementById('uploadWrapper').style.display = 'none';
+}
